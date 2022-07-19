41380c8e05f5fb35fb97f66f3fec2f6c24e01315
carlton <carltonmbaj0@gmail.com>
2022-07-19 18:37:22 +0300

    Assignment complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,23 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = isDarkMode ? "App dark" : "App light"
+  const [isDark, setIsDark] = useState(false);
+  const appClass = isDark ? "App dark" : "App light";
 
-  function handleChange() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+  function darkToggle(event) {
+    setIsDark((isDark) => !isDark);
   }
-
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button onClick={handleChange}>Dark Mode</button>
+        <button onClick={darkToggle}>
+          {isDark ? "Dark Mode" : "Light Mode"}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,18 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
-  const [isInCart, setIsInCart] = useState(false);
-
-  function handleAddToCart(){
-    setIsInCart((isInCart) => !isInCart)
+  const [inCart, setInCart] = useState(true);
+  const inCartClass = inCart ? "" : "in-cart";
+  function addItemToCart(event) {
+    setInCart((inCart) => !inCart);
   }
   return (
-    <li className={isInCart ? "in-cart" : ""}>
+    <li className={inCartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button onClick={handleAddToCart} className="add">{isInCart ? "Remove From" : "Add to"} Cart</button>
+      <button className="add" onClick={addItemToCart}>
+        {inCart ? "Add To Cart" : "Remove From Cart"}
+      </button>
     </li>
   );
 }
